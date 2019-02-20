9d175a2bb2db7ab8eb0db84ab9a56ae33dfa4f3a
SaphireVert <45922476+SaphireVert@users.noreply.github.com>
2019-02-20 18:44:33 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flèche
+{
+    public class Classefleche
+    {
+        public int Taillefleche = 19;
+
+
+
+        public void fonctionfleche()
+        {
+
+
+            int i;
+            for (i = 0; i < Taillefleche - 1; i++)
+            {
+
+                Console.Write(StringRepetition(Taillefleche - i - 1, " "));
+                Console.Write("*");
+
+                Console.Write(StringRepetition(i * 2 - 1, " "));
+
+                if (i != 0)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine(StringRepetition(Taillefleche * 2 - 1, "*"));
+
+            for(i = 0; i < Taillefleche; i++)
+            {
+                Console.WriteLine(StringRepetition(Taillefleche - 1, " ") + "*");
+            }
+
+        }
+
+        public string StringRepetition(int No, string caractere)
+        {
+            string s = string.Empty;
+
+            int i;
+            for(i = 0; i < No; i++)
+            {
+                s = s + caractere;
+
+            }
+            return s;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flèche
+{
+    class Program
+    {
+
+        
+        //cki
+        static void Main(string[] args)
+        {
+            while (true == true)
+            {
+                Console.Clear();
+            Console.WriteLine("Bienvenue dans \"Flèches\", saisissez un nombre entre 2 et 20 et appuyez sur Entrée");
+            string touchetemp = Console.ReadLine();
+            //int nbtouche = int.Parse(touchetemp);
+            if(touchetemp.Length > 0)
+                {
+                    if (int.Parse(touchetemp) > 2 && int.Parse(touchetemp) < 20 && touchetemp.Length > 0)
+                    {
+                        Classefleche classefleche = new Classefleche();
+                        classefleche.Taillefleche = int.Parse(touchetemp);
+                        classefleche.fonctionfleche();
+                        Console.WriteLine("Appuyez sur une touche pour continuer...");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Erreur... Le nombre " + int.Parse(touchetemp) + " ne se situe pas entre 2 et 20");
+                        Console.WriteLine("Appuyez sur une touche pour continuer...");
+                        Console.ReadKey();
+
+                    }
+                }
+
+
+
+
+            }
+
+
+        }
+    }
+}
